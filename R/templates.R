d6d401a140b995f3e3d3a6af4dc9ea40566e91db
andrewpbray <andrewbray@berkeley.edu>
2020-10-08 17:17:36 -0700

    initial commit

---
+++ R/templates.R
@@ -0,0 +1,13 @@
+#' Class commons
+#'
+#' A classroom space to engage with course content in a social manner. Designed
+#' for roughly 24 students plus one instructor.
+#'
+#' @format A list with a bunch of stuff in it:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"class_commons"
