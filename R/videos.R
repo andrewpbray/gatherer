beb0610ad4240c00d2eead8aeac16613984b1743
andrewpbray <andrewbray@berkeley.edu>
2020-10-09 00:10:22 -0700

    initial commit

---
+++ R/videos.R
@@ -0,0 +1,22 @@
+#' Add videos module
+#'
+#' @description
+#'
+#' @return A character vector of the available templates.
+#' @export
+add_videos <- function(obj) {
+}
+
+
+#' Edit videos module
+#'
+#' @description
+#'
+#' @return A character vector of the available templates.
+#' @export
+edit_videos <- function(obj, ) {
+}
+
+remove_videos <- function(obj) {
+
+}
